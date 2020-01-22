afda1a2a1725c71c5bc8883de5a9c7468f0f8810
Victor <victor.bedmar@atos.net>
2020-01-22 13:29:47 +0100

    se cambia el nombre del proyecto comunes de usuario

--- springboot-servicio-usuarios/src/main/java/com/curso/springboot/app/usuarios/RepositoryConfig.java
+++ springboot-servicio-usuarios/src/main/java/com/curso/springboot/app/usuarios/RepositoryConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-import com.curso.springboot.app.usuarios.models.entity.Role;
-import com.curso.springboot.app.usuarios.models.entity.Usuario;
+import com.curso.springboot.app.usercommons.models.entity.Role;
+import com.curso.springboot.app.usercommons.models.entity.Usuario;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer{
--- springboot-servicio-usuarios/src/main/java/com/curso/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
+++ springboot-servicio-usuarios/src/main/java/com/curso/springboot/app/usuarios/SpringbootServicioUsuariosApplication.java
@@ -2,9 +2,10 @@ package com.curso.springboot.app.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-//@EntityScan({"com.curso.springboot.app.usuarios.commons.models.entity"})
+@EntityScan({"com.curso.springboot.app.usercommons.models.entity"})
 public class SpringbootServicioUsuariosApplication {
 
 	public static void main(String[] args) {
